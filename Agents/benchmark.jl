19822018e49be63ac279c1affe47469585fcf4db
Peter Heywood <peethwd@gmail.com>
2022-11-03 17:13:12 +0000

    Disable exeuction of WolfSheep and ForestFire for now, in the interest of runtime

--- Agents/benchmark.jl
+++ Agents/benchmark.jl
@@ -2,19 +2,19 @@ using Agents
 using BenchmarkTools
 using Test
 
-a = @benchmark step!(model, agent_step!, model_step!, 500) setup = (
-    (model, agent_step!, model_step!) = Models.predator_prey(
-        n_wolves = 40,
-        n_sheep = 60,
-        dims = (25, 25),
-        Δenergy_sheep = 5,
-        Δenergy_wolf = 13,
-        sheep_reproduce = 0.2,
-        wolf_reproduce = 0.1,
-        regrowth_time = 20,
-    )
-) samples = 100
-println("Agents.jl WolfSheep (ms): ", minimum(a.times) * 1e-6)
+# a = @benchmark step!(model, agent_step!, model_step!, 500) setup = (
+#     (model, agent_step!, model_step!) = Models.predator_prey(
+#         n_wolves = 40,
+#         n_sheep = 60,
+#         dims = (25, 25),
+#         Δenergy_sheep = 5,
+#         Δenergy_wolf = 13,
+#         sheep_reproduce = 0.2,
+#         wolf_reproduce = 0.1,
+#         regrowth_time = 20,
+#     )
+# ) samples = 100
+# println("Agents.jl WolfSheep (ms): ", minimum(a.times) * 1e-6)
 
 a = @benchmark step!(model, agent_step!, model_step!, 100) setup = (
     (model, agent_step!, model_step!) = Models.flocking(
@@ -33,7 +33,7 @@ a = @benchmark step!(model, agent_step!, model_step!, 10) setup = (
 ) samples = 100
 println("Agents.jl Schelling (ms): ", minimum(a.times) * 1e-6)
 
-a = @benchmark step!(model, agent_step!, model_step!, 100) setup =
-    ((model, agent_step!, model_step!) = Models.forest_fire())
-println("Agents.jl ForestFire (ms): ", minimum(a.times) * 1e-6)
+# a = @benchmark step!(model, agent_step!, model_step!, 100) setup =
+#     ((model, agent_step!, model_step!) = Models.forest_fire())
+# println("Agents.jl ForestFire (ms): ", minimum(a.times) * 1e-6)
 
