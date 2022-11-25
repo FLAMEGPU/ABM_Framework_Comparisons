a9524e17cca0b0182b5500289456261b811715c0
Peter Heywood <peethwd@gmail.com>
2022-11-25 16:16:07 +0000

    Agents: Do not use @benchmark due to OOM killing for large step counts of large runs
    This required enabling a higher than default number of seconds timeouts, to get multiple runs to actually occur. Even when enabling gc between repetitions it was consuming > 22GiB.

--- Agents/benchmark.jl
+++ Agents/benchmark.jl
@@ -1,13 +1,13 @@
 using Pkg; Pkg.instantiate()
 
 using Agents
-using BenchmarkTools
 using Test
 using Statistics
 
 SAMPLE_COUNT = 10
 
-# a = @benchmark step!(model, agent_step!, model_step!, 500) setup = (
+# times = []
+# for i in 0:SAMPLE_COUNT
 #     (model, agent_step!, model_step!) = Models.predator_prey(
 #         n_wolves = 40,
 #         n_sheep = 60,
@@ -18,11 +18,21 @@ SAMPLE_COUNT = 10
 #         wolf_reproduce = 0.1,
 #         regrowth_time = 20,
 #     )
-# ) samples = SAMPLE_COUNT
-# println("Agnets.jl WolfSheep times (ms)", map(x -> x * 1e-6, a.times))
-# println("Agents.jl WolfSheep (mean ms): ", (Statistics.mean(a.times)) * 1e-6)
+#     step_stats = @timed step!(model, agent_step!, model_step!, 500) 
+#     # Time the steps! function, capturing the timing info
+#     step_stats = @timed step!(model, agent_step!, model_step!, 1) 
+#     # discard the 0th repetitions time, due to JIT, save other times for alter
+#     if i > 0
+#         append!(times, step_stats.time)
+#     end
+# end
+# println("Agnets.jl WolfSheep times (ms)", map(x -> x * 1e3, times))
+# println("Agents.jl WolfSheep (mean ms): ", (Statistics.mean(times)) * 1e3)
 
-a = @benchmark step!(model, agent_step!, model_step!, 100) setup = (
+# Don't use @benchmark for multiple long runs - it eats and eats memory even with gc enabled?
+# Run 1 to N times, discarding the first timing.
+times = []
+for i in 0:SAMPLE_COUNT
     (model, agent_step!, model_step!) = Models.flocking(
         n_birds = 100000,
         separation = 1,
@@ -32,19 +42,38 @@ a = @benchmark step!(model, agent_step!, model_step!, 100) setup = (
         visual_distance = 5.0,
         extent = (1000, 1000),
     )
-) samples = SAMPLE_COUNT
-println("Agnets.jl Flocking times (ms)", map(x -> x * 1e-6, a.times))
-println("Agents.jl Flocking (mean ms): ", (Statistics.mean(a.times)) * 1e-6)
+    step_stats = @timed step!(model, agent_step!, model_step!, 100)
+    if i > 0
+        append!(times, step_stats.time)
+    end
+end
+println("Agnets.jl Flocking times (ms)", map(x -> x * 1e3, times))
+println("Agents.jl Flocking (mean ms): ", (Statistics.mean(times)) * 1e3)
 
-a = @benchmark step!(model, agent_step!, model_step!, 100) setup = (
-    (model, agent_step!, model_step!) =
-        Models.schelling(griddims = (500, 500), numagents = 200000)
-) samples = SAMPLE_COUNT
-println("Agnets.jl schelling times (ms)", map(x -> x * 1e-6, a.times))
-println("Agents.jl Schelling (mean ms): ", (Statistics.mean(a.times)) * 1e-6)
-
-# a = @benchmark step!(model, agent_step!, model_step!, 100) setup =
-#     ((model, agent_step!, model_step!) = Models.forest_fire())
-# println("Agnets.jl ForestFire times (ms)", map(x -> x * 1e-6, a.times))
-# println("Agents.jl ForestFire (mean ms): ", (Statistics.mean(a.times)) * 1e-6)
+# Don't use @benchmark for multiple long runs - it eats and eats memory even with gc enabled?
+# Run 1 to N times, discarding the first timing.
+times = []
+for i in 0:SAMPLE_COUNT
+    (model, agent_step!, model_step!) = Models.schelling(griddims = (500, 500), numagents = 200000)
+    step_stats = @timed step!(model, agent_step!, model_step!, 100)
+    if i > 0
+        append!(times, step_stats.time)
+    end
+end
+println("Agnets.jl schelling times (ms)", map(x -> x * 1e3, times))
+println("Agents.jl Schelling (mean ms): ", (Statistics.mean(times)) * 1e3)
+# # Don't use @benchmark for multiple long runs - it eats and eats memory even with gc enabled?
+# # Run 1 to N times, discarding the first timing.
+# times = []
+# for i in 0:SAMPLE_COUNT
+#     (model, agent_step!, model_step!) = Models.forest_fire()
+#     # Time the steps! function, capturing the timing info
+#     step_stats = @timed step!(model, agent_step!, model_step!, 1) 
+#     # discard the 0th repetitions time, due to JIT, save other times for alter
+#     if i > 0
+#         append!(times, step_stats.time)
+#     end
+# end
+# println("Agnets.jl ForestFire times (ms)", map(x -> x * 1e3, times))
+# println("Agents.jl ForestFire (mean ms): ", (Statistics.mean(times)) * 1e3)
 
